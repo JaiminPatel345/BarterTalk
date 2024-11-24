f37e717f5f3cc1c791c56bf8eb45e7cbfa1e4b53
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2024-11-24 10:49:06 +0530

    start for frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes , Route } from "react-router-dom"
+import { Home, Signup, Login } from "./pages"
+
 function App() {
     return (
         <>
-            <h1 className="bg-rose-300">Working</h1>
+            <div className="min-h-screen flex justify-center items-center">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </div>
         </>
     )
 }
